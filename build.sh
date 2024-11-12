efa4e4883a65809c4ecb294e13dfc634aa453750
Jason Florentino <jason.florentino@relayfi.com>
2024-11-11 19:07:20 -0500

    Build stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-GOOS=linux GOARCH=amd64 go build -v -o ./out/main ./cmd
+echo "Building Go"
+GO_OUT_FILE="./out/main"
+time GOOS=linux GOARCH=amd64 go build -v -o $GO_OUT_FILE ./cmd
+echo "Wrote to $GO_OUT_FILE"
 
+echo ""
+echo "Building CSS"
+CSS_OUT_FILE="./static/style.css"
 # You need tailwind CLI:
 # https://tailwindcss.com/blog/standalone-cli
-./tailwindcss -i ./css/style.css -o ./static/style.css --minify
+time ./tailwindcss -i ./css/style.css -o $CSS_OUT_FILE --minify
+echo "Wrote to $CSS_OUT_FILE"
+
+echo ""
+echo "Finished"
