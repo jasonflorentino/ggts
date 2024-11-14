a4f46a3769b53759fd07cd48d46053c40dda92de
Jason Florentino <jason.florentino@relayfi.com>
2024-11-13 19:01:47 -0500

    Add back env from file

--- lib/env/env.go
+++ lib/env/env.go
@@ -3,7 +3,33 @@ package env
 import (
 	"log"
 	"os"
+	"strings"
 )
+
+var env map[string]string
+
+func LoadEnv() {
+	const FILE_NAME string = ".env"
+	const KV_SEP string = "="
+	env = make(map[string]string)
+	file, err := os.ReadFile(FILE_NAME)
+	if err != nil {
+		log.Fatal("Error loading env files")
+	}
+	lines := strings.Split(string(file), "\n")
+	for i, line := range lines {
+		kv := strings.Split(line, KV_SEP)
+		if len(kv) > 2 {
+			log.Fatalf("env line %d splits to more than 2 items: %v", i, kv)
+		}
+		if strings.HasPrefix(kv[1], "\"") {
+			log.Fatalf("quoted vals not supported: env line %d - %v", i, kv[1])
+		}
+		keyParts := strings.Split(kv[1], " ") // anything after the first space is ignored
+		env[kv[0]] = keyParts[0]
+	}
+	assertEnv()
+}
 
 func assertEnv() {
 	vars := []string{
@@ -11,46 +37,20 @@ func assertEnv() {
 		"GGTS_PORT",
 	}
 	for _, v := range vars {
-		if v := os.Getenv(v); v == "" {
+		if v := env[v]; v == "" {
 			log.Fatalf("Expected %s to be set in .env", v)
 		}
 	}
 }
 
-func LoadEnv() {
-	// This did not work on my linux build running on Ubuntu 22.04
-	// with a go1.23.3.linux-amd64 install, but maybe it's just
-	// just because I'm always doing this at midnight ><
-	//
-	// const FILE_NAME string = ".env"
-	// const KV_SEP string = "="
-	// file, err := os.ReadFile(FILE_NAME)
-	// if err != nil {
-	// 	log.Fatal("Error loading env files")
-	// }
-	// lines := strings.Split(string(file), "\n")
-	// for i, line := range lines {
-	// 	kv := strings.Split(line, KV_SEP)
-	// 	if len(kv) > 2 {
-	// 		log.Fatalf("env line %d splits to more than 2 items: %v", i, kv)
-	// 	}
-	// 	if strings.HasPrefix(kv[1], "\"") {
-	// 		log.Fatalf("quoted vals not supported: env line %d - %v", i, kv[1])
-	// 	}
-	// 	keyParts := strings.Split(kv[1], " ") // anything after the first space is ignored
-	// 	os.Setenv(kv[0], keyParts[0])
-	// }
-	assertEnv()
-}
-
 func IsProd() bool {
-	return os.Getenv("GGTS_ENV") == "production"
+	return env["GGTS_ENV"] == "production"
 }
 
 func NotProd() bool {
-	return os.Getenv("GGTS_ENV") != "production"
+	return env["GGTS_ENV"] != "production"
 }
 
 func Port() string {
-	return os.Getenv("GGTS_PORT")
+	return env["GGTS_PORT"]
 }
